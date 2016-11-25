d3860646f928f402db37e4894387fb84db3f69fc
junifar <junifar@gmail.com>
2016-11-25 18:02:05 +0700

    Change into from load SyncLoader Implementation - Still Not Works yet

--- app/src/main/java/com/rubahapi/myshow/LatestMovieActivity.java
+++ app/src/main/java/com/rubahapi/myshow/LatestMovieActivity.java
@@ -1,11 +1,10 @@
 package com.rubahapi.myshow;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -14,20 +13,9 @@ import android.support.v7.widget.RecyclerView;
 import com.rubahapi.myshow.adapter.MovieAdapter;
 import com.rubahapi.myshow.data.MovieDBHelper;
 import com.rubahapi.myshow.data.MovieProvider;
-import com.rubahapi.myshow.model.MovieURL;
-import com.rubahapi.myshow.pojo.popular.PopularMovie;
-import com.rubahapi.myshow.pojo.popular.Result;
 import com.rubahapi.myshow.service.sync.MovieSyncAdapter;
-import com.rubahapi.myshow.singleton.GsonSingleton;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.List;
-
-public class LatestMovieActivity extends AppCompatActivity {
+public class LatestMovieActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
 //    ArrayList<Contact> contacts;
 //    ContactsAdapter contactsAdapter;
@@ -51,7 +39,22 @@ public class LatestMovieActivity extends AppCompatActivity {
 
         rvContacts.setLayoutManager(new GridLayoutManager(this,2));
         movieAdapter = new MovieAdapter(null);
+//        movieAdapter = new MovieAdapter();
         rvContacts.setAdapter(movieAdapter);
+
+        getLoaderManager().initLoader(MOVIE_LOADER, null, this).forceLoad();
+
+//        Uri uri = Uri.parse("content://" + MovieProvider.CONTENT_AUTHORITY + "/movie");
+//        Cursor cursor = (Cursor) new CursorLoader(LatestMovieActivity.this,
+//                uri,
+//                new String[]{
+//                        MovieDBHelper.COLUMN_IMAGE_PATH
+//                },
+//                null,
+//                null,
+//                null);
+//
+//        movieAdapter.updateResult(cursor);
 
         MovieSyncAdapter.SyncStart(this);
 
@@ -87,84 +90,109 @@ public class LatestMovieActivity extends AppCompatActivity {
 //        getContentResolver().notifyChange(uri, null);
 //    }
 
-    public boolean isOnline() {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
+//    public boolean isOnline() {
+//        ConnectivityManager cm =
+//                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+//        return netInfo != null && netInfo.isConnectedOrConnecting();
+//    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        Uri uri = Uri.parse("content://" + MovieProvider.CONTENT_AUTHORITY + "/movie");
+        if (i == MOVIE_LOADER){
+            return new CursorLoader(LatestMovieActivity.this,
+                    uri,
+                    new String[]{
+                            MovieDBHelper.COLUMN_IMAGE_PATH
+                    },
+                    null,
+                    null,
+                    null);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        movieAdapter.updateResult(cursor);
+        movieAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        movieAdapter.updateResult(null);
     }
 
 
-    class getPopularMoviesTask extends AsyncTask<Void, Void, PopularMovie>{
-        @Override
-        protected void onPostExecute(PopularMovie popularMovie) {
-            super.onPostExecute(popularMovie);
-//            contactsAdapter.updateContacts(popularMovie);
-//            contactsAdapter.notifyDataSetChanged();
-            movieAdapter.updateResult(popularMovie.getResults());
-            movieAdapter.notifyDataSetChanged();
-        }
-
-        @Override
-        protected PopularMovie doInBackground(Void... voids) {
-            HttpURLConnection conn = null;
-            try{
-                URL url = new URL(MovieURL.getPopularMovie());
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(5000);
-                conn.setConnectTimeout(5000);
-                conn.setRequestMethod("GET");
-                conn.connect();
-
-                int responseCode = conn.getResponseCode();
-                if(responseCode != HttpURLConnection.HTTP_OK){
-                    return null;
-                }
-
-                InputStream inputStream = conn.getInputStream();
-                if(null == inputStream){
-                    return null;
-                }
-
-                StringBuilder stringBuilder = new StringBuilder();
-
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String strLine;
-
-                while ((strLine = bufferedReader.readLine()) != null){
-                    stringBuilder.append(strLine);
-                }
-
-                PopularMovie popularMovie = GsonSingleton.getGson().fromJson(stringBuilder.toString(), PopularMovie.class);
-                List<Result> results = popularMovie.getResults();
-                ContentValues[] contentValues = new ContentValues[results.size()];
-                for (int i=0; i<results.size(); i++){
-                    Result result = results.get(i);
-                    ContentValues cv = new ContentValues();
-                    cv.put(MovieDBHelper.COLUMN_TITLE, result.getTitle());
-                    cv.put(MovieDBHelper.COLUMN_DESCRIPTION, result.getOverview());
-                    cv.put(MovieDBHelper.COLUMN_YEARS, result.getReleaseDate());
-                    cv.put(MovieDBHelper.COLUMN_IMAGE_PATH,"http://image.tmdb.org/t/p/w185/" + result.getPosterPath());
-                    contentValues[i] = cv;
-                }
-
-                Uri uri = Uri.parse("content://" + MovieProvider.CONTENT_AUTHORITY + "/movie");
-                getContentResolver().delete(uri,null,null);
-                getContentResolver().bulkInsert(
-                        uri,
-                        contentValues
-                );
-                getContentResolver().notifyChange(uri, null);
-                return popularMovie;
-            } catch (Exception e){
-                return null;
-            } finally {
-                if (null != conn){
-                    conn.disconnect();
-                }
-            }
-//            return null;
-        }
-    }
+//    class getPopularMoviesTask extends AsyncTask<Void, Void, PopularMovie> {
+//        @Override
+//        protected void onPostExecute(PopularMovie popularMovie) {
+//            super.onPostExecute(popularMovie);
+//            movieAdapter.updateResult(popularMovie.getResults());
+//            movieAdapter.notifyDataSetChanged();
+//        }
+//
+//        @Override
+//        protected PopularMovie doInBackground(Void... voids) {
+//            HttpURLConnection conn = null;
+//            try{
+//                URL url = new URL(MovieURL.getPopularMovie());
+//                conn = (HttpURLConnection) url.openConnection();
+//                conn.setReadTimeout(5000);
+//                conn.setConnectTimeout(5000);
+//                conn.setRequestMethod("GET");
+//                conn.connect();
+//
+//                int responseCode = conn.getResponseCode();
+//                if(responseCode != HttpURLConnection.HTTP_OK){
+//                    return null;
+//                }
+//
+//                InputStream inputStream = conn.getInputStream();
+//                if(null == inputStream){
+//                    return null;
+//                }
+//
+//                StringBuilder stringBuilder = new StringBuilder();
+//
+//                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//                String strLine;
+//
+//                while ((strLine = bufferedReader.readLine()) != null){
+//                    stringBuilder.append(strLine);
+//                }
+//
+//                PopularMovie popularMovie = GsonSingleton.getGson().fromJson(stringBuilder.toString(), PopularMovie.class);
+//                List<Result> results = popularMovie.getResults();
+//                ContentValues[] contentValues = new ContentValues[results.size()];
+//                for (int i=0; i<results.size(); i++){
+//                    Result result = results.get(i);
+//                    ContentValues cv = new ContentValues();
+//                    cv.put(MovieDBHelper.COLUMN_TITLE, result.getTitle());
+//                    cv.put(MovieDBHelper.COLUMN_DESCRIPTION, result.getOverview());
+//                    cv.put(MovieDBHelper.COLUMN_YEARS, result.getReleaseDate());
+//                    cv.put(MovieDBHelper.COLUMN_IMAGE_PATH,"http://image.tmdb.org/t/p/w185/" + result.getPosterPath());
+//                    contentValues[i] = cv;
+//                }
+//
+//                Uri uri = Uri.parse("content://" + MovieProvider.CONTENT_AUTHORITY + "/movie");
+//                getContentResolver().delete(uri,null,null);
+//                getContentResolver().bulkInsert(
+//                        uri,
+//                        contentValues
+//                );
+//                getContentResolver().notifyChange(uri, null);
+//                return popularMovie;
+//            } catch (Exception e){
+//                return null;
+//            } finally {
+//                if (null != conn){
+//                    conn.disconnect();
+//                }
+//            }
+////            return null;
+//        }
+//    }
 }
--- app/src/main/java/com/rubahapi/myshow/adapter/MovieAdapter.java
+++ app/src/main/java/com/rubahapi/myshow/adapter/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.rubahapi.myshow.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.rubahapi.myshow.R;
-import com.rubahapi.myshow.pojo.popular.Result;
 import com.squareup.picasso.Picasso;
-
-import java.util.List;
 
 /**
  * Created by prasetia on 11/24/2016.
@@ -22,14 +20,28 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public static final int TYPE_ITEM = 1;
 
-    private List<Result> results;
+//    private List<Result> results;
 
-    public MovieAdapter(List<Result> results) {
-        this.results = results;
+    Cursor cursor;
+
+//    public  MovieAdapter(){
+//        this.results = null;
+//    }
+//
+//    public MovieAdapter(List<Result> results) {
+//        this.results = results;
+//    }
+
+    public MovieAdapter(Cursor cursor) {
+        this.cursor = cursor;
     }
 
-    public void updateResult(List<Result> results){
-        this.results = results;
+//    public void updateResult(List<Result> results){
+//        this.results = results;
+//    }
+
+    public void updateResult(Cursor cursor){
+        this.cursor = cursor;
     }
 
     @Override
@@ -45,16 +57,28 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     public void onBindViewHolder(ViewHolder holder, int position) {
         Context context = holder.imageView.getContext();
 
-        Result result = results.get(position);
+//        if (null != results){
+//            Result result = results.get(position);
+//
+//            Picasso.with(context).load("http://image.tmdb.org/t/p/w185/" + result.getPosterPath()).into(holder.imageView);
+//        }
 
-        Picasso.with(context).load("http://image.tmdb.org/t/p/w185/" + result.getPosterPath()).into(holder.imageView);
+        if(null != cursor){
+            cursor.moveToPosition(position);
+            Picasso.with(context).load("http://image.tmdb.org/t/p/w185/" + cursor.getString(0));
+        }
     }
 
     @Override
     public int getItemCount() {
-        if(null != results){
-            return  results.size();
+//        if(null != results){
+//            return  results.size();
+//        }
+
+        if (null != cursor){
+            return  cursor.getCount();
         }
+
         return 0;
     }
 
